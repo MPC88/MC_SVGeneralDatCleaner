504a56b83ee349a656cd655e5c523388a74cdef4
MPC88 <myemail@gmail.com>
2023-08-24 21:59:42 +0100

    Add project files.

---
+++ MC_SVGeneralDatCleaner/Control.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MC_SVGeneralDatCleaner
+{
+    internal class Control
+    {
+        private const string EXE = "\\Star Valor.exe";
+        private const string GENERALDAT = "\\Star Valor_Data\\general.dat";
+        private const string ASSEMBLY = "\\Star Valor_Data\\Managed\\Assembly-CSharp.dll";
+
+        internal List<int> ShipList { get { return shipList; } }
+
+        private static string path = "";
+        private List<int> shipList = new();
+        private object? generalData;
+
+        internal bool SetPath(string path)
+        {
+            if (!File.Exists(path + EXE))
+                return false;
+
+            Control.path = path;
+            return true;
+        }
+
+        internal bool DeserialiseGeneraDat()
+        {
+            FileStream fileStream = null;
+            try
+            {
+                BinaryFormatter binaryFormatter = new();
+                fileStream = File.Open(path + GENERALDAT, FileMode.Open);
+                binaryFormatter.Binder = new GeneralDataBinder();
+                generalData = binaryFormatter.Deserialize(fileStream);
+
+                Assembly starValorAss = Assembly.LoadFrom(path + ASSEMBLY);
+
+                Type? generaldataType = starValorAss.GetType("GeneralData");
+                if (generaldataType == null)
+                    return false;
+
+                FieldInfo? generalDatShips = generaldataType.GetField("spaceships");
+                if (generalDatShips == null)
+                    return false;
+
+                object? list = generalDatShips.GetValue(generalData);
+                if (list == null)
+                    return false;
+
+                this.shipList = (List<int>)list;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (fileStream != null)
+                    fileStream.Close();
+            }
+            return true;
+        }
+
+        internal bool SerialiseGeneralDat(string savePath)
+        {
+            FileStream fileStream = null;
+            try
+            {
+                Assembly starValorAss = Assembly.LoadFrom(path + ASSEMBLY);
+
+                Type? generaldataType = starValorAss.GetType("GeneralData");
+                if (generaldataType == null)
+                    return false;
+
+                FieldInfo? generalDatShips = generaldataType.GetField("spaceships");
+                if (generalDatShips == null)
+                    return false;
+
+                generalDatShips.SetValue(generalData, shipList);
+
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                fileStream = File.Create(savePath);
+                binaryFormatter.Serialize(fileStream, generalData);
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (fileStream != null)
+                    fileStream.Close();
+            }
+            return true;
+        }
+
+        internal bool RemoveShip(int id)
+        {
+            if (shipList == null)
+                return false;
+
+            if (!shipList.Contains(id))
+                return false;
+
+            shipList.Remove(id);
+            return true;
+        }
+
+        private class GeneralDataBinder : SerializationBinder
+        {
+            public override Type? BindToType(string assemblyName, string typeName)
+            {
+                if (typeName.Equals("GeneralData"))
+                {
+                    Assembly starValorAss = Assembly.LoadFrom(path + ASSEMBLY);
+
+                    Type? generaldataType = starValorAss.GetType("GeneralData");
+                    if (generaldataType == null)
+                        return null;
+
+                    return generaldataType;
+                }
+                else
+                {
+                    return typeof(List<int>);
+                }
+            }
+        }
+    }
+}
---
+++ MC_SVGeneralDatCleaner/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MC_SVGeneralDatCleaner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Control control;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            control = new Control();
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void starvalorpath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (starvalorpath.Text == "")
+                return;
+
+            if (!control.SetPath(starvalorpath.Text))
+            {
+                MessageBox.Show("Star Valor.exe could not be found in the selected path.");
+                starvalorpath.Text = "";
+            }
+        }
+
+        private void pathbrowse_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                if(result == System.Windows.Forms.DialogResult.OK)
+                    starvalorpath.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void load_Click(object sender, RoutedEventArgs e)
+        {
+            if (control.DeserialiseGeneraDat())
+            {
+                listBox.ItemsSource = control.ShipList;
+                listBox.Items.Refresh();
+            }
+            else
+                MessageBox.Show("Failed to load general.dat.  Ensure you have set the correct path to the Star Valor folder.");
+        }
+
+        private void listBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Delete)
+                DeleteItem();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteItem();
+        }
+
+        private void DeleteItem()
+        {
+            if (listBox.SelectedItem == null)
+                return;
+
+            if(control.RemoveShip((int)listBox.SelectedItem))
+            {
+                listBox.ItemsSource = control.ShipList;
+                listBox.Items.Refresh();
+            }
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.SaveFileDialog())
+            {
+                dialog.Filter = "Data Files (*.dat)|*.dat";
+                dialog.FileName = "general.dat";
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                    if (!control.SerialiseGeneralDat(dialog.FileName))
+                        MessageBox.Show("Failed to save general.dat");
+            }
+        }
+    }
+}
